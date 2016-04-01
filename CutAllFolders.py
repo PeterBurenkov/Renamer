abfc5a9b50510623c2765023ad5280fbbd2f1864
Petro <p.burenkov@levi9.com>
2016-04-01 18:40:07 +0300

    Changes in date

--- CutAllFolders.py
+++ CutAllFolders.py
@@ -2,11 +2,11 @@ from Renamer import cut
 from os.path import isdir
 import datetime
 
-date = datetime.datetime(2016, 1, 3)
-dateLimit = datetime.datetime(2016, 3, 29)
+date = datetime.datetime(2016, 01, 01)
+dateLimit = datetime.datetime(2016, 05, 01)
 while True:
     folderName = datetime.datetime.strftime(date, '%Y%m%d')
-    folder = "d:\\photo\\picturesWork\\" + folderName
+    folder = "d:\\photo\\picturesHome\\" + folderName
     if isdir(folder):
         #print(date, folderName, 'yes')
         cut(folder)
--- ShiftAllFolders.py
+++ ShiftAllFolders.py
@@ -2,14 +2,14 @@ from Renamer import shift
 from os.path import isfile, join, isdir
 import datetime
 
-date = datetime.datetime(2016, 2, 2)
-dateLimit = datetime.datetime(2016, 3, 29)
+date = datetime.datetime(2016, 3, 25)
+dateLimit = datetime.datetime(2016, 3, 30)
 while True:
     folderName = datetime.datetime.strftime(date, '%Y%m%d')
-    folder = "d:\\photo\\picturesWork\\" + folderName
+    folder = "d:\\photo\\picturesHome\\" + folderName
     if isdir(folder):
-        #print(date, folderName, 'yes')
-        shift(folder, 25)
+        #print(folder, folderName, 'yes')
+        shift(folder, -60)
 
     date = date + datetime.timedelta(days=1)
     if date > dateLimit:
